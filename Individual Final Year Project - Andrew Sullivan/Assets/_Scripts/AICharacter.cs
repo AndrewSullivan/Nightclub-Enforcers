4e05fc8a7459cdffcd51c6697ba5313f54de1289
AndrewSullivan <andrewsullivan121@outlook.com>
2022-02-04 00:29:24 +0000

    Added another AI state for walking towards the bar & playing a new drunk animation when at the bar

--- Individual Final Year Project - Andrew Sullivan/Assets/_Scripts/AICharacter.cs
+++ Individual Final Year Project - Andrew Sullivan/Assets/_Scripts/AICharacter.cs
@@ -9,8 +9,10 @@ public class AICharacter : MonoBehaviour
 
     NavMeshAgent aiNavMeshAgent;
 
-    public float distanceFromTestTarget;
-    public Transform testTarget;
+    public float distanceFromDanceFloor;
+    public float distanceFromBar;
+    public Transform danceFloor;
+    public Transform bar;
 
     // Start is called before the first frame update
     void Start()
@@ -26,13 +28,23 @@ public class AICharacter : MonoBehaviour
         // Get's current animation state of character.
         AnimatorStateInfo aiAnimStateInfo = aiAnim.GetCurrentAnimatorStateInfo(0);
 
-        // Calculates the distance between the player and target, stores the value in a float variable.
-        distanceFromTestTarget = Vector3.Distance(testTarget.position, transform.position);
-        aiAnim.SetFloat("DistFromTarget", distanceFromTestTarget); // Sets the float value of the animator parameter "DistFromTarget" to whatever value is being stored in distanceFromTestTarget float variable.
+        // Calculates the distance between the player and dance floor, stores the value in a float variable.
+        distanceFromDanceFloor = Vector3.Distance(danceFloor.position, transform.position);
+        aiAnim.SetFloat("DistFromDanceFloor", distanceFromDanceFloor); // Sets the float value of the animator parameter "DistFromDanceFloor" to whatever value is being stored in distanceFromDanceFloor float variable.
+
+        // Calculates the distance between the player and bar, stores the value in a float variable.
+        distanceFromBar = Vector3.Distance(bar.position, transform.position);
+        aiAnim.SetFloat("DistFromBar", distanceFromBar); // Sets the float value of the animator parameter "DistFromBar" to whatever value is being stored in distanceFromBar float variable.
 
         if (aiAnimStateInfo.IsName("ElizabethWalking")) // If it's current animator state is "Walking", it will set it's destination to the test target objects position.
         {
-            aiNavMeshAgent.SetDestination(testTarget.position);
+            aiNavMeshAgent.SetDestination(danceFloor.position);
+            aiNavMeshAgent.isStopped = false;
+        }
+
+        if (aiAnimStateInfo.IsName("ElizabethWalking2")) // If it's current animator state is "Walking", it will set it's destination to the test target objects position.
+        {
+            aiNavMeshAgent.SetDestination(bar.position);
             aiNavMeshAgent.isStopped = false;
         }
 
