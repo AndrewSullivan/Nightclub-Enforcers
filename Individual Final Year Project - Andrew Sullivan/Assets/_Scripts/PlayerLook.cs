1d6ee3d237194ca50f97c985252b80ad7cbcfa39
AndrewSullivan <andrewsullivan121@outlook.com>
2022-03-12 12:52:31 +0000

    Added Puddle Material & Plane (Needs to be fixed)

--- Individual Final Year Project - Andrew Sullivan/Assets/_Scripts/PlayerLook.cs
+++ Individual Final Year Project - Andrew Sullivan/Assets/_Scripts/PlayerLook.cs
@@ -8,17 +8,25 @@ public class PlayerLook : MonoBehaviour
 
     public Transform player;
 
+    public float mouseXRot = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked; // Locks mouse pointer to the centre of the screen.
     }
 
     // Update is called once per frame
     void Update()
     {
         float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
 
-        player.Rotate(Vector3.up * mouseX);
+        mouseXRot -= mouseY;
+        mouseXRot = Mathf.Clamp(mouseXRot, -90f, 90f); // This clamps the mouse look on the y-axis, player can not look up or down past 90 degrees.
+
+
+        transform.localRotation = Quaternion.Euler(mouseXRot, 0f, 0f); // Allows the camera to rotate on the y-axis.
+        player.Rotate(Vector3.up * mouseX); // Rotates the camera on the x-axis.
     }
 }
