31263028001dd8c1eb0cb9cd369ce2a8f9e252b2
AndrewSullivan <andrewsullivan121@outlook.com>
2022-04-02 18:45:25 +0100

    Latest Version (Need to fix bar ai)

--- Individual Final Year Project - Andrew Sullivan/Assets/_Scripts/AI/AIScript.cs
+++ Individual Final Year Project - Andrew Sullivan/Assets/_Scripts/AI/AIScript.cs
@@ -7,9 +7,11 @@ public class AIScript : MonoBehaviour
 {
     NavMeshAgent aiAgent;
     public Transform danceFloor;
+    public Transform bar;
     public Animator aiAnim;
 
-    public float distance;
+    public float distanceDanceFloor;
+    public float distanceBar;
     bool isMoving;
     bool isIdle;
 
@@ -23,13 +25,26 @@ public class AIScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distance = Vector3.Distance(danceFloor.position, this.transform.position);
 
-        aiAnim.SetFloat("DistFromDanceFloor", distance);
+        // Dance Floor
+        distanceDanceFloor = Vector3.Distance(danceFloor.position, this.transform.position);
 
-        if (distance < 25f)
+        aiAnim.SetFloat("DistFromDanceFloor", distanceDanceFloor);
+
+        if (distanceDanceFloor < 25f)
         {
             aiAgent.SetDestination(danceFloor.position);
+            isMoving = true;
+        }
+
+        // Bar
+        distanceBar = Vector3.Distance(bar.position, this.transform.position);
+
+        aiAnim.SetFloat("DistFromBar", distanceBar);
+
+        if (distanceBar < 25f)
+        {
+            aiAgent.SetDestination(bar.position);
             isMoving = true;
         }
 
