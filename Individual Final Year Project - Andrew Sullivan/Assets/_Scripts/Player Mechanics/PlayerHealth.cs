316bba4d6d84ee108403ec6f0ef697007a568b01
AndrewSullivan <andrewsullivan121@outlook.com>
2022-04-03 16:33:40 +0100

    Added player health system

---
+++ Individual Final Year Project - Andrew Sullivan/Assets/_Scripts/Player Mechanics/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int playerHealth;
+    public GameObject endGameCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerHealth <= 0)
+        {
+            endGameCanvas.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "AI")
+        {
+            playerHealth = playerHealth - 10;
+        }
+    }
+}
